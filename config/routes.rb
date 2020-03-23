a64d775b43dd15a00f32eca2194948796a6959b9
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-03-23 20:47:08 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tickets
   root to: "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
