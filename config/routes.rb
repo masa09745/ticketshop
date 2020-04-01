3ce4cc13b610613a058f6aad58d1c177bebe3d87
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-04-02 01:30:08 +0900

    userpageに必要なファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   }
   resources :tickets
   resources :schedules,:stadiums, only: [:index, :show]
+
+  scope :mypage do
+    resources :users, only: [:show]
+  end
+
   root to: "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
