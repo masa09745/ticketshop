817fdcba2890815017d073f631cc9d3c5e02bed7
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-04-04 18:00:02 +0900

    スケジュールデータ追加とビューの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,61 @@ Stadium.create!(
 
 Schedule.create!(
   [
-    {match_name:'A VS B', match_date: '2020-04-01 18:00'},
-    {match_name:'C VS D', match_date: '2020-04-01 18:00'},
-    {match_name:'E VS F', match_date: '2020-04-01 18:00'},
-    {match_name:'A VS B', match_date: '2020-04-02 18:00'},
-    {match_name:'C VS D', match_date: '2020-04-02 18:00'},
-    {match_name:'E VS F', match_date: '2020-04-02 18:00'},
-    {match_name:'A VS B', match_date: '2020-04-03 18:00'},
-    {match_name:'C VS D', match_date: '2020-04-03 18:00'},
-    {match_name:'E VS F', match_date: '2020-04-03 18:00'}
+    {match_name:'チームA VS チームB', match_date: '2020-03-31 18:00'},
+    {match_name:'チームC VS チームD', match_date: '2020-03-31 18:00'},
+    {match_name:'チームE VS チームF', match_date: '2020-03-31 18:00'},
+    {match_name:'チームA VS チームB', match_date: '2020-04-01 18:00'},
+    {match_name:'チームC VS チームD', match_date: '2020-04-01 18:00'},
+    {match_name:'チームE VS チームF', match_date: '2020-04-01 18:00'},
+    {match_name:'チームA VS チームB', match_date: '2020-04-02 18:00'},
+    {match_name:'チームC VS チームD', match_date: '2020-04-02 18:00'},
+    {match_name:'チームE VS チームF', match_date: '2020-04-02 18:00'},
+
+    {match_name:'チームA VS チームC', match_date: '2020-04-03 18:00'},
+    {match_name:'チームB VS チームE', match_date: '2020-04-03 18:00'},
+    {match_name:'チームD VS チームF', match_date: '2020-04-03 18:00'},
+    {match_name:'チームA VS チームC', match_date: '2020-04-04 14:00'},
+    {match_name:'チームB VS チームE', match_date: '2020-04-04 14:00'},
+    {match_name:'チームD VS チームF', match_date: '2020-04-04 14:00'},
+    {match_name:'チームA VS チームC', match_date: '2020-04-05 14:00'},
+    {match_name:'チームB VS チームE', match_date: '2020-04-05 14:00'},
+    {match_name:'チームD VS チームF', match_date: '2020-04-05 14:00'},
+
+    {match_name:'チームA VS チームD', match_date: '2020-04-07 18:00'},
+    {match_name:'チームB VS チームF', match_date: '2020-04-07 18:00'},
+    {match_name:'チームC VS チームE', match_date: '2020-04-07 18:00'},
+    {match_name:'チームA VS チームD', match_date: '2020-04-08 18:00'},
+    {match_name:'チームB VS チームF', match_date: '2020-04-08 18:00'},
+    {match_name:'チームC VS チームE', match_date: '2020-04-08 18:00'},
+    {match_name:'チームA VS チームD', match_date: '2020-04-09 18:00'},
+    {match_name:'チームB VS チームF', match_date: '2020-04-09 18:00'},
+    {match_name:'チームC VS チームE', match_date: '2020-04-09 18:00'},
+
+
+    {match_name:'チームA VS チームF', match_date: '2020-04-11 14:00'},
+    {match_name:'チームB VS チームC', match_date: '2020-04-11 14:00'},
+    {match_name:'チームD VS チームE', match_date: '2020-04-11 14:00'},
+    {match_name:'チームA VS チームE', match_date: '2020-04-12 14:00'},
+    {match_name:'チームA VS チームE', match_date: '2020-04-12 14:00'},
+    {match_name:'チームA VS チームE', match_date: '2020-04-12 14:00'},
+
+    {match_name:'チームB VS チームA', match_date: '2020-04-14 18:00'},
+    {match_name:'チームD VS チームC', match_date: '2020-04-14 18:00'},
+    {match_name:'チームF VS チームE', match_date: '2020-04-14 18:00'},
+    {match_name:'チームB VS チームA', match_date: '2020-04-15 18:00'},
+    {match_name:'チームD VS チームC', match_date: '2020-04-15 18:00'},
+    {match_name:'チームF VS チームE', match_date: '2020-04-15 18:00'},
+    {match_name:'チームB VS チームA', match_date: '2020-04-16 18:00'},
+    {match_name:'チームD VS チームC', match_date: '2020-04-16 18:00'},
+    {match_name:'チームF VS チームE', match_date: '2020-04-16 18:00'},
+
+    {match_name:'チームD VS チームA', match_date: '2020-04-18 14:00'},
+    {match_name:'チームF VS チームB', match_date: '2020-04-18 18:00'},
+    {match_name:'チームE VS チームC', match_date: '2020-04-18 14:00'},
+    {match_name:'チームD VS チームA', match_date: '2020-04-19 18:00'},
+    {match_name:'チームF VS チームB', match_date: '2020-04-19 18:00'},
+    {match_name:'チームE VS チームC', match_date: '2020-04-19 18:00'},
+
 
   ]
 
