e2210fb91c5f298a25cf266dcada500a352b5bde
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-04-28 01:30:06 +0900

    チーム→スケジュール→詳細までのサーバサイド再構築

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,19 +1,19 @@
 class SchedulesController < ApplicationController
-  before_action :set_match, only: :show
+  before_action :set_team, only: [:index, :show]
 
   def index
-    @team = Team.find(params[:team_id])
     @schedules = Schedule.where(team1_id:[params[:team_id]]).or(Schedule.where(team2_id:[params[:team_id]]))
+
   end
 
   def show
-    @schedule = Schedule.all
+    @schedule = Schedule.find(params[:id])
 
   end
 
   private
 
-  def set_match
-    @match = Schedule.find(params[:id])
+  def set_team
+    @team = Team.find(params[:team_id])
   end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -8,4 +8,5 @@ class Schedule < ApplicationRecord
 
   belongs_to :team1, class_name: 'Team', foreign_key: 'team1_id'
   belongs_to :team2, class_name: 'Team', foreign_key: 'team2_id'
+  belongs_to :venue
 end
