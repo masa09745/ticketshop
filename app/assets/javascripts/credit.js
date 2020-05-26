6ef12e75b5d79891d0dd7807518211d99e63e1f5
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-05-27 02:50:37 +0900

    カード登録画面のフォーム修正とJSの修正

--- app/assets/javascripts/credit.js
+++ app/assets/javascripts/credit.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function() {
-  var form = $("#charge-form"),
-      number = form.find('input[name="number"]'),
-      cvc = form.find('input[name="cvc"]'),
+  var form = $("#credit-form"),
+      number = form.find('input[name="credit_number"]'),
+      cvc = form.find('input[name="credit_code"]'),
       exp_month = form.find('select[name="exp_month"]'),
-      exp_year = form.find('input[name="exp_year"]');
+      exp_year = form.find('select[name="exp_year"]');
 
   $("#charge-form").submit(function() {
     form.find("input[type=submit]").prop("disabled", true);
@@ -20,11 +20,8 @@ $(document).on('turbolinks:load', function() {
         form.find('button').prop('disabled', false);
       }
       else {
-        $(".number").removeAttr("name");
-        $(".cvc").removeAttr("name");
-        $(".exp_month").removeAttr("name");
-        $(".exp_year").removeAttr("name");
-
+        $(".form-control").removeAttr("name");
+        
         var token = response.id;
 
         form.append($('<input type="hidden" name="payjpToken" />').val(token));
