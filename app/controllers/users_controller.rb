bea790396641c7329af790379b64a47e68a8363e
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-06-06 21:58:52 +0900

    ユーザーが所持しているtikctの表示(非同期はしない)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
   end
+
+  def ticket
+    @tickets = Ticket.where(user_id: current_user.id).includes(:order)
+  end
+
 end
